b1692ef59cd0f444890f318bbf460ef11b2e6feb
andbeyond0331 <mjpark1329@naver.com>
2022-08-20 23:49:03 +0900

    2022-08-20 내일 회의다! 파이팅!

---
+++ src/main/java/algorithm/baekjun/sprout/print/Pro11718/Main.java
@@ -0,0 +1,16 @@
+package main.java.algorithm.baekjun.sprout.print.Pro11718;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+		String str;
+		while((str=(bufferedReader.readLine()))!=null) {
+			System.out.println(str);
+		}
+		bufferedReader.close();
+	}
+}
