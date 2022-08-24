d9ac347922a33413d430960a48e242aa9f2895ec
andbeyond0331 <mjpark1329@naver.com>
2022-08-24 22:18:26 +0900

    2022-08-24 정신차리자!!! 며칠을 넘긴거야!!!

---
+++ src/main/java/algorithm/baekjun/Class/level3/Pro1927/Main.java
@@ -0,0 +1,28 @@
+package main.java.algorithm.baekjun.Class.level3.Pro1927;
+
+//2022-08-24 최소힙
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(bufferedReader.readLine());
+		PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
+		for (int i = 0; i < N; i++) {
+			int n = Integer.parseInt(bufferedReader.readLine());
+			if(n==0){
+				if(pq.isEmpty()){
+					System.out.println(0);
+				}else{
+					System.out.println(pq.poll());
+				}
+			}else{
+				pq.add(n);
+			}
+		}
+	}
+}
