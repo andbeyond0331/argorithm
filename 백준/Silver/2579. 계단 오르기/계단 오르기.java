a0254500a2f1b4ed462cc72af4bb5f235f1dbf1d
PARK MI JUNG <88870452+andbeyond0331@users.noreply.github.com>
2024-04-21 07:54:37 +0900

    [Silver III] Title: 계단 오르기, Time: 124 ms, Memory: 14232 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.java
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -1,40 +1,35 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-	static Integer[][] dp;
-	static int[] stairs;
 	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int T = Integer.parseInt(br.readLine());
-		stairs = new int[T];
-		for (int i = 0; i < T; i++) {
-			stairs[i] = Integer.parseInt(br.readLine());
+		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(bufferedReader.readLine());
+		int[][] dp = new int[T][3]; // dp[i][j]는 연속으로 j번 밟고 T일 때의 최대 점수
+		int[] stairs = new int[T];
+		for(int i = 0; i < T; i++) {
+			stairs[i] = Integer.parseInt(bufferedReader.readLine());
 		}
-		dp = new Integer[T][3];
-
-		dp[0][0] = 0;			 // 안밟
-		dp[0][1] = stairs[0];	 // 연속 밟
-		dp[0][2] = stairs[0];	 // 안연속 밟
-		if (T == 1) {
+		dp[0][0] = 0;
+		dp[0][1] = stairs[0];
+		dp[0][2] = stairs[0];
+		if(T==1) {
 			System.out.println(stairs[0]);
 			return;
 		}
-		dp[1][0] = stairs[0];
-		dp[1][1] = stairs[0] + stairs[1];
-		dp[1][2] = stairs[1];
-		if (T == 2) {
-			System.out.println(dp[1][1]);
-			return;
+		dp[1][0] = dp[0][1];
+		dp[1][1] = stairs[1];
+		dp[1][2] = stairs[1] + dp[0][1];
+		if(T==2) {
+			System.out.println(dp[1][2]);
+            return;
 		}
 		for(int i = 2; i < T; i++) {
 			dp[i][0] = Math.max(dp[i-1][1], dp[i-1][2]);
-			dp[i][1] = dp[i-1][2] + stairs[i];
-			dp[i][2] = Math.max(dp[i-2][1], dp[i-2][2]) + stairs[i];
+			dp[i][1] = Math.max(dp[i-2][1], dp[i-2][2]) + stairs[i];
+			dp[i][2] = dp[i-1][1] + stairs[i];
 		}
 		System.out.println(Math.max(dp[T-1][1], dp[T-1][2]));
-
 	}
 }
