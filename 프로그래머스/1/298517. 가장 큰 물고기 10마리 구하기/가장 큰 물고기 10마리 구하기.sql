b2c446b682fe2226c079848e39497eca490fea4a
PARK MI JUNG <88870452+andbeyond0331@users.noreply.github.com>
2024-04-21 17:09:17 +0900

    [level 1] Title: 가장 큰 물고기 10마리 구하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/1/298517. 가장 큰 물고기 10마리 구하기/가장 큰 물고기 10마리 구하기.sql
@@ -0,0 +1,6 @@
+-- 코드를 작성해주세요
+SELECT t.id as ID, f.length as LENGTH
+from (select rank() over (order by length desc) r, id
+     from fish_info) t, fish_info f
+where t.r <= 10 and t.id = f.id
+order by 2 desc, 1 asc;
