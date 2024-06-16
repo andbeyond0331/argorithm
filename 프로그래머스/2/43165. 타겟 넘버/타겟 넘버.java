f214be54aeb82fda222873392cc3d59ec80429ae
PARK MI JUNG <88870452+andbeyond0331@users.noreply.github.com>
2024-06-16 10:07:41 +0900

    [level 2] Title: 타겟 넘버, Time: 31.23 ms, Memory: 88.6 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,56 +1,51 @@
 class Solution {
-    // 마이너스 개수 0부터 5
-    // 하나씩 넣어보고 개수 다 차면 return
-    static int n = 0;
+    static int count;
     static boolean[] visited;
+    static int tg;
     static int[] nums;
-    static int ans = 0;
-    static int tar = 0;
     public int solution(int[] numbers, int target) {
-        int answer = 0;
-        n = numbers.length;
-        visited = new boolean[n];
-        nums = numbers;
-        tar = target;
-        for(int i = 0; i < n; i++) {
-            dfs(i, 0, 0);
+        visited = new boolean[numbers.length];
+        count = 0;
+        tg = target;
+        nums = new int[numbers.length];
+        // 빼기 갯수 정하기 (0~N)
+        for(int i = 0; i < numbers.length; i++) {
+            nums[i] = numbers[i];
         }
-        answer = ans;
-        return answer;
+        for(int i = 0; i < numbers.length; i++) {
+            decide(0, 0, i);
+        }
+        // 위치 정하기
+        return count;
     }
-    
-    static void dfs(int amount, int current, int depth) {
-        // System.out.println("amount: " + amount + ", current  : " + current + ", depth: " + depth);
-        if(amount == current) {
-            int tmp = 0;
-            for(int i = 0; i < nums.length; i ++) {
-                if(visited[i]) {
-                    tmp-=nums[i];
-                }else{
-                    tmp+=nums[i];
-                }
-            }
-            if(tmp == tar) {
-                // System.out.println(depth);
-                // for(boolean b: visited) {
-                //     System.out.print(b + " ");
-                // }
-                
-                ans ++;
-            }
+    static void decide(int cur, int depth, int min) {
+        if(depth == min) {
+            calculate();
             return;
         }
-        // visited[depth] = true;
-        // dfs(amount, current+1, depth+1);
-        // visited[depth] = false;
-        // dfs(amount, current+1, depth);
-        for(int i = depth; i< nums.length; i++) {
+        for(int i = cur;i < nums.length; i++) {
             if(!visited[i]) {
                 visited[i] = true;
-                dfs(amount, current+1, i+1);
+                decide(i+1, depth+1, min);
                 visited[i] = false;
             }
         }
-        
+    }
+    static void calculate() {
+        int sum = 0;
+        String a = "";
+        for(int i = 0; i < nums.length; i++) {
+            if(visited[i]) {
+                sum-=nums[i];
+                a+="-"+nums[i];
+            }else{
+                sum+=nums[i];
+                a+="+"+nums[i];
+            }
+        }
+        // System.out.println(a);
+        if(sum==tg) {
+            count++;
+        }
     }
 }
