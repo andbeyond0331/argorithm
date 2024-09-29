c40fdb199d9b99b2f4239540421bd4b40fd446c7
PARK MI JUNG <88870452+andbeyond0331@users.noreply.github.com>
2024-09-29 21:13:51 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 1036 ms, Memory: 164868 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -1,45 +1,37 @@
-import java.io.*;
-import java.util.*;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
 
 public class Main {
-
-    public static void main(String[] args) throws IOException {
-
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringBuffer stringBuffer = new StringBuffer();
-
-        int N = Integer.parseInt(bufferedReader.readLine());
-        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine()," ");
-        Map<Integer,Integer> sgCards = new LinkedHashMap<>();
-
-        for (int i = 0; i < N; i++) {
-            int temp = Integer.parseInt(stringTokenizer.nextToken());
-            if(sgCards.containsKey(temp)){
-                int count = sgCards.get(temp);
-                
-                sgCards.replace(temp, count+1);
-
-            }else{
-                sgCards.put(temp,1);
-            }
-
-        }
-
-        int M = Integer.parseInt(bufferedReader.readLine());
-        StringTokenizer stringTokenizer1 = new StringTokenizer(bufferedReader.readLine(), " ");
-
-        for (int i = 0; i < M; i++) {
-            int temp = Integer.parseInt(stringTokenizer1.nextToken());
-            if(sgCards.containsKey(temp)){
-                stringBuffer.append(sgCards.get(temp)).append(" ");
-            }else{
-                stringBuffer.append(0).append(" ");
-            }
-        }
-        bufferedWriter.write(String.valueOf(stringBuffer));
-        bufferedWriter.flush();
-        bufferedWriter.close();
-        bufferedReader.close();
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int N = Integer.parseInt(br.readLine());
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    Map<Integer, Integer> map = new HashMap<>();
+    for(int i = 0;i < N; i++) {
+      int temp = Integer.parseInt(st.nextToken());
+      if(map.containsKey(temp)) {
+        map.put(temp, map.get(temp)+1);
+      }else {
+        map.put(temp, 1);
+      }
     }
+    int M = Integer.parseInt(br.readLine());
+    st = new StringTokenizer(br.readLine());
+    StringBuilder sb = new StringBuilder();
+    for(int i = 0; i < M; i++) {
+      int temp = Integer.parseInt(st.nextToken());
+      int ans = map.containsKey(temp)?map.get(temp):0;
+      if(i==M-1) {
+        sb.append(ans);
+      }else{
+        sb.append(ans).append(" ");
+      }
+    }
+    System.out.println(sb.toString());
+  }
 }
