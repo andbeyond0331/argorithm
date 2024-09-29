cd2d21308b5e40950653fcb46f00c97bc3905809
PARK MI JUNG <88870452+andbeyond0331@users.noreply.github.com>
2024-09-29 17:27:04 +0900

    [level 2] Title: 전화번호 목록, Time: 337.58 ms, Memory: 98.3 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -1,29 +1,37 @@
-import java.util.Arrays;
+import java.util.*;
 
 class Solution {
     public boolean solution(String[] phone_book) {
         boolean answer = true;
         Arrays.sort(phone_book);
-        for(int i = 0; i < phone_book.length-1; i++) {
+        for(int i = 0;i < phone_book.length-1; i++) {
             if(phone_book[i+1].startsWith(phone_book[i])) {
-                return false;
-            }
+                answer = false;
+                break;
+            } 
         }
-//         for(int i = 0; i < phone_book.length-1; i++) {
-//             for(int j = i+1; j < phone_book.length; j++) {
+//         outer: for(int i= 0; i < phone_book.length-1; i++) {
+//             String a = phone_book[i];
+//             for(int j = i + 1; j < phone_book.length; j++) {
+//                 String b = phone_book[j];
                 
-                // if(phone_book[i].length()!=phone_book[j].length() && phone_book[i].substring(0,1).equals(phone_book[j].substring(0,1))) {
-                    // String shorter = phone_book[i].length()<phone_book[j].length()? phone_book[i]: phone_book[j];
-                    // String longer = phone_book[i].length()<phone_book[j].length()?phone_book[j]:phone_book[i];
-                    // if(longer.startsWith(shorter)) {
-                    //     return false;
-                    // }
-//                     if(shorter.equals(longer.substring(0, shorter.length()))){
-//                         return false;
-
+//                 if(a.length()<b.length()) {
+//                     if(b.startsWith(a)) {
+//                         answer = false;
+//                         break outer;
 //                     }
-                // }
-                
+//                 }
+//             }
+//         }
+        
+        // outer: for(int i = 0;i < phone_book.length-1; i++) {
+        //     String a = phone_book[i];
+        //     for(int j = i+1; j < phone_book.length; j++) {
+        //         String b = phone_book[j];
+        //         if(a.startsWith(b) || b.startsWith(a)) {
+        //             answer = false;
+        //             break outer;
+        //         }
         //     }
         // }
         return answer;
