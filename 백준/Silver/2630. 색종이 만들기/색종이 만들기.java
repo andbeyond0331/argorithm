767d8963a07919fd5a47a6c9296e47e802e8e540
PARK MI JUNG <88870452+andbeyond0331@users.noreply.github.com>
2024-10-13 19:25:47 +0900

    [Silver II] Title: 색종이 만들기, Time: 132 ms, Memory: 15588 KB -BaekjoonHub

--- 백준/Silver/2630. 색종이 만들기/색종이 만들기.java
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.java
@@ -2,254 +2,92 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-
-    public static int countB;
-    public static int countW;
-
-    public static void main(String[] args) throws IOException {
-
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-
-        int N= Integer.parseInt(bufferedReader.readLine());
-
-        int[][] paper = new int[N][N];
-        countB=0;
-        countW = 0;
-
-        for (int i = 0; i < N; i++) {
-            StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine()," ");
-            for (int j = 0; j < N; j++) {
-                paper[i][j] = Integer.parseInt(stringTokenizer.nextToken());
-            }
+  static int[][] paper;
+  static int white;
+  static int blue;
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int N = Integer.parseInt(br.readLine());
+    StringTokenizer st;
+    white = 0;
+    blue = 0;
+    paper = new int[N][N];
+    for(int i = 0;i < N ;i++) {
+      st = new StringTokenizer(br.readLine());
+      for(int j = 0; j < N; j++) {
+        paper[i][j] = Integer.parseInt(st.nextToken());
+      }
+    }
+    find(0, N-1, 0, N-1);
+    System.out.println(white);
+    System.out.println(blue);
+  }
+  static void find(int sx, int ex, int sy, int ey ){
+//    System.out.println("sx = " + sx + ", ex = " + ex + ", sy = " + sy + ", ey = " + ey);
+    if(sx==ex && sy==ey) {
+      if(paper[sx][sy]==1) {
+        blue++;
+      }else{
+        white++;
+      }
+      return;
+    }
+    boolean flag = false;
+    int temp = paper[sx][sy];
+    outer:for(int i = sx; i<=ex; i++) {
+      for(int j = sy; j <=ey;j++) {
+        if(paper[i][j]!=temp) {
+          flag = true;
+          break outer;
         }
-
-//        System.out.println("paper : " + Arrays.toString(paper));
-//        for (int i = 0; i < N; i++) {
-//            System.out.println("paper : " + Arrays.toString(paper[i]));
+      }
+    }
+    if(flag) {
+      find(sx, (sx+ex)/2, sy, (sy+ey)/2);
+      find(sx, (sx+ex)/2, (sy+ey)/2+1, ey);
+      find((sx+ex)/2+1, ex, sy, (sy+ey)/2);
+      find((sx+ex)/2+1, ex, (sy+ey)/2+1, ey);
+    }else{
+      if(temp == 1) {
+        blue++;
+      }else{
+        white++;
+      }
+    }
+  }
+//  static void cut(int sx, int ex, int sy, int ey) {
+//    if(sx==ex && sy==ey) {
+//      if(paper[sx][sy] == 1) {
+//        blue++;
+//      }else{
+//        white++;
+//      }
+//      return;
+//    }
+//    int temp = -1;
+//    int startX = 0;
+//    int startY = 0;
+//    for(int i = sx; i <= ex; i++) {
+//      for(int j = sy; j<=ey; j++) {
+//        if(temp==-1) {
+//          temp = paper[i][j];
+//          startX = i;
+//          startY = j;
+//        }else {
+//          if(temp != paper[i][j]) {
+//            if(i==0) {
+//              cut(startX, startX, )
+//            }
+//            cut(startX, i-1, startY, j-1);
+//            temp = -1;
+//            i = i-1;
+//            j = j-1;
+//          }
 //        }
-
-        divide(paper, 0, N-1, 0, N-1);
-
-        System.out.println(countW);
-        System.out.println(countB);
-
-
-
-    }
-
-    public static void divide(int[][] paper, int left, int right, int top, int bottom){
-        if( left == right && top == bottom){
-            if(paper[left][top]==1){
-                countB ++;
-            }else{
-                countW++;
-            }
-            return;
-        }
-        int midR = (left + right) / 2;
-        int midC = (top + bottom) / 2;
-        int[] sum = conquer(paper, left, midR, right, top, midC, bottom);
-        if(sum[2]==1 && sum[3]==1 && sum[4]==1 && sum[5]==1){
-                if(sum[0]==4){
-                    countW += 1;
-                }else if(sum[1]==4){
-                    countB += 1;
-                }else{
-                    countW+=sum[0];
-                    countB+=sum[1];
-                }
-            }else{
-                countW +=sum[0];
-                countB +=sum[1];
-                if(sum[2]==1){
-
-                }else if(sum[2]==-1){
-
-                    divide(paper, left, midR, top, midC);
-                }else{
-                    System.out.println("22...읭?");
-                }
-                if(sum[3]==1){
-
-                }else if(sum[3]==-1){
-
-                    divide(paper, left, midR, midC+1, bottom);
-                }else{
-                    System.out.println("33...읭?");
-                }
-                if(sum[4]==1){
-
-                }else if(sum[4]==-1){
-
-                    divide(paper, midR+1, right, top, midC);
-                }else{
-                    System.out.println("44...읭?");
-                }
-                if(sum[5]==1){
-
-                }else if(sum[5]==-1){
-
-                    divide(paper, midR+1, right, midC+1, bottom);
-                }else{
-                    System.out.println("55...읭?");
-                }
-
-            }
-
-
-
-
-
-
-    }
-
-
-    public static int[] conquer(int[][] paper, int left, int midR, int right, int top, int midC, int bottom){
-//        int l = left;
-//        int r = midR +1;
-//        int t = top;
-//        int b = midC +1;
-
-//        System.out.println("left = " + left + ", midR = " + midR + ", right = " + right + ", top = " + top + ", midC = " + midC + ", bottom = " + bottom);
-        int idxR = left;
-        int idxC = top;
-
-        int[] result = {0,0,0,0,0,0};
-
-        if(left==right && top == bottom){
-            if(paper[idxR][idxC]==0){
-//                System.out.println("WWWWWWWWW");
-//                System.out.println("left = " + left + ", midR = " + midR + ", right = " + right + ", top = " + top + ", midC = " + midC + ", bottom = " + bottom);
-//                System.out.println("WWWWWWWWW");
-                result[0] = -1;
-            }else{
-//                System.out.println("BBBBBBBBB");
-//                System.out.println("left = " + left + ", midR = " + midR + ", right = " + right + ", top = " + top + ", midC = " + midC + ", bottom = " + bottom);
-//                System.out.println("BBBBBBBBB");
-                result[1] = -1;
-
-            }
-        }else{
-
-            int targetA =paper[idxR][idxC];
-
-            while(idxR<=midR && idxC<=midC){
-                if(idxR==midR && idxC==midC){
-                    if(paper[idxR][idxC]==targetA){
-                        result[2] = 1;
-                        if(targetA == 0){
-                            result[0] ++;
-                        }else{
-                            result[1]++;
-                        }
-                    }
-                }
-
-                if(paper[idxR][idxC]!=targetA){
-                    result[2] = -1;
-                    break;
-                }
-                if(idxR<midR){
-                    idxR++;
-                }else{
-                    idxR=left;
-                    idxC++;
-                }
-
-            }
-            idxR = left;
-            idxC = midC+1;
-
-            int targetB = paper[idxR][idxC];
-
-            while(idxR<=midR && idxC<=bottom){
-                if(idxR==midR && idxC==bottom){
-                    if(paper[idxR][idxC]==targetB){
-                        result[3] = 1;
-
-                        if(targetB == 0){
-                            result[0] ++;
-                        }else{
-                            result[1]++;
-                        }
-                    }
-                }
-
-                if(paper[idxR][idxC]!=targetB){
-                    result[3] = -1;
-                    break;
-                }
-                if(idxR<midR){
-                    idxR++;
-                }else{
-                    idxR=left;
-                    idxC++;
-                }
-
-            }
-            idxR = midR+1;
-            idxC = top;
-            int targetC = paper[idxR][idxC];
-
-            while(idxR<=right && idxC<=midC){
-                if(idxR==right && idxC==midC){
-                    if(paper[idxR][idxC]==targetC){
-                        result[4] = 1;
-                        if(targetC == 0){
-                            result[0] ++;
-                        }else{
-                            result[1]++;
-                        }
-                    }
-                }
-
-                if(paper[idxR][idxC]!=targetC){
-                    result[4] = -1;
-                    break;
-                }
-                if(idxR<right){
-                    idxR++;
-                }else{
-                    idxR=midR+1;
-                    idxC++;
-                }
-
-            }
-            idxR = midR+1;
-            idxC = midC+1;
-            int targetD = paper[idxR][idxC];
-
-            while(idxR<=right && idxC<=bottom){
-                if(idxR==right && idxC==bottom){
-                    if(paper[idxR][idxC]==targetD){
-                        result[5] = 1;
-                        if(targetD == 0){
-                            result[0] ++;
-                        }else{
-                            result[1]++;
-                        }
-                    }
-                }
-
-                if(paper[idxR][idxC]!=targetD){
-                    result[5] = -1;
-                    break;
-                }
-                if(idxR<right){
-                    idxR++;
-                }else{
-                    idxR=midR+1;
-                    idxC++;
-                }
-
-            }
-
-        }
-
-        return result;
-    }
+//      }
+//    }
+//  }
 }
