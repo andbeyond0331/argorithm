46251cbd9fbc2c66608e88369396389456cb70e1
PARK MI JUNG <88870452+andbeyond0331@users.noreply.github.com>
2024-10-20 11:30:17 +0900

    [Silver I] Title: IOIOI, Time: 232 ms, Memory: 20184 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.java
+++ 백준/Silver/5525. IOIOI/IOIOI.java
@@ -1,53 +1,34 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Main {
-  public static void main(String[] args) throws IOException {
-    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
-    int N = Integer.parseInt(bufferedReader.readLine());
-    int M = Integer.parseInt(bufferedReader.readLine());
-    String S = bufferedReader.readLine();
-    // 탐색하고 있는 위치 i 이전에 찾은 적 있으면 이후 OI 가 있다면 추가 & 다음 탐색은 +2부터
-    // 없으면 그 위치부터 IOIOI.. 반복되는지 확인
-    boolean flag = false;
-    int ans = 0;
-    outer: for(int i = 0;i < M-1; i++) {
-      if (flag) { // 바로 이전에 발견됨.
-        if(2*N+1+(i-1)+1>M-1) {
-          break;
-        }else{
-          if(S.charAt(2*N+1+(i-1))=='O'&& S.charAt(2*N+1+(i-1)+1)=='1') {
-            ans++;
-            i++;
-          }else {
-            flag = false;
-            continue;
-          }
-        }
-      }else{
-        if(i+(2*N)>M-1) {
-          break;
-        }else{
-          for(int j = 0;j < 2*N+1; j++) {
-            if(j%2==0) {
-              if(S.charAt(i+j)!='I') {
-                continue outer;
-              }
-            }else{
-              if(S.charAt(i+j)!='O') {
-                continue outer;
-              }
+        int N = Integer.parseInt(bufferedReader.readLine()); // 'N'개의 IOI 패턴
+        int M = Integer.parseInt(bufferedReader.readLine()); // 문자열의 길이
+        String S = bufferedReader.readLine(); // 입력 문자열
+
+        int patternCount = 0; // N개의 "IOI" 패턴을 찾은 개수
+        int ans = 0; // 답
+
+        for (int i = 0; i < M - 1; i++) {
+            // "IOI" 패턴이 시작되는지 확인
+            if (S.charAt(i) == 'I') {
+                int count = 0;
+                while (i + 2 < M && S.charAt(i + 1) == 'O' && S.charAt(i + 2) == 'I') {
+                    count++; // "IOI" 패턴이 몇 번 반복되는지 카운트
+                    i += 2; // "IOI"가 반복될 때마다 인덱스를 2씩 증가
+
+                    if (count == N) { // N개의 "IOI" 패턴이 완성되면
+                        ans++;
+                        count--; // 다음 "IOI" 패턴을 찾기 위해 중간 값은 유지
+                    }
+                }
             }
-          }
-          ans++;
-          flag = true;
         }
-      }
+
+        System.out.println(ans);
     }
-    System.out.println(ans);
-  }
 }
